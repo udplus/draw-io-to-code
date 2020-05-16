5c56320b1fc4d5e0b3c07173763d2e0daae0d555
Fraser Tooth <frasertooth@gmail.com>
2020-05-16 23:06:44 +0900

    Interfaces

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,43 @@ const writeFileAsync = promisify(fs.writeFile);
 
 const parser = new xml2js.Parser();
 
+interface IDrawIOMXCell {
+  $: IDrawIOMXCellProperties; //Properties of the Cell
+  mxGeometry?: IDrawIOMXGeometryCell; //Child
+}
+
+//Highly Dependant on which cell is present
+interface IDrawIOMXCellProperties {
+  id: string;
+  style?: string;
+  parent?: string;
+  source?: string;
+  target?: string;
+  edge?: string;
+  vertex?: string;
+}
+
+interface IDrawIOMXGeometryCell {
+  $: IDrawIOMXGeometryCellProperties;
+}
+interface IDrawIOMXGeometryCellProperties {
+  x?: string;
+  y?: string;
+  width?: string;
+  height?: string;
+  relative?: string;
+  as: string;
+}
+
 export const loadDiagramCells = async (filePath: String): Promise<any[]> => {
   const res = await readFileAsync(filePath);
   const xmlObject = await parser.parseStringPromise(res);
   return xmlObject.mxfile.diagram[0].mxGraphModel[0].root[0].mxCell;
+};
+
+const buildFileFromCells = (cells: IDrawIOMXCell[]): String => {
+  console.log(cells);
+  return "blah";
 };
 
 export const generateFunctionFromDiagram = async (
@@ -22,7 +55,7 @@ export const generateFunctionFromDiagram = async (
   const writeFilePath = "dist/" + filePath.split("/")[1].split(".")[0] + ".js";
   const writtenFile = await writeFileAsync(
     writeFilePath,
-    "function identityFunction(input) {\n  return input;\n}\n"
+    buildFileFromCells(cells)
   );
   return Promise.all([writtenFile]);
 };

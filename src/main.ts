87c1188429b6390b031ce37fe0956ebd70a37e45
Fraser Tooth <frasertooth@gmail.com>
2020-05-16 18:24:45 +0900

    Imported XML

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log("Hello");
+const fs = require("fs");
+const xml2js = require("xml2js");
+const { promisify } = require("util");
+
+const readFileAsync = promisify(fs.readFile);
+
+const parser = new xml2js.Parser();
+
+export const loadXML = async (filePath: String) => {
+  const res = await readFileAsync(filePath);
+  console.log("File Loaded");
+  parser.parseString(res, function (err: any, result: any) {
+    console.dir(result.mxfile.diagram[0].mxGraphModel[0].root[0].mxCell);
+    console.log("Done");
+  });
+};
+
+loadXML("test_diagrams/simpleConsoleLogFunction.drawio");
